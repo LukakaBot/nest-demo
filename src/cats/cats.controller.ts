acf79f892e1a260c268f64b9bacee3fdf47be2bf
Lukaka <monishine_code@163.com>
2024-09-10 17:09:31 +0800

    feat: Res decorator

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,43 +1,58 @@
 import {
-  Query,
-  Body,
+  // Query,
+  // Body,
   Controller,
   Get,
-  Param,
+  // Param,
   Post,
-  Put,
-  Delete,
+  // Put,
+  // Delete,
+  HttpStatus,
+  Res,
 } from '@nestjs/common';
-import { CreateCatDto } from './create-cat.dto';
-import { ListAllEntities } from '../common/common.dto';
-import { UpdateCatDto } from './update-cat.dto';
+// import { CreateCatDto } from './create-cat.dto';
+// import { ListAllEntities } from '../common/common.dto';
+// import { UpdateCatDto } from './update-cat.dto';
+import { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(@Query() query: ListAllEntities): string {
-    return `This action return all cats (limit: ${query.limit} items)`;
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string): string {
-    return `This action returns a #${id} cat`;
+  findAll(@Res() res: Response) {
+    res.status(HttpStatus.OK).json([]);
   }
 
   @Post()
-  create(@Body() createCatDto: CreateCatDto): string {
-    console.log(createCatDto);
-    return `This action adds a new cat`;
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto): string {
-    console.log(updateCatDto);
-    return `This action updates a #${id} cat`;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string): string {
-    return `This action removes a #${id} cat`;
+  create(@Res() res: Response) {
+    res.status(HttpStatus.CREATED).send();
   }
 }
+
+// export class CatsController {
+//   @Get()
+//   findAll(@Query() query: ListAllEntities): string {
+//     return `This action return all cats (limit: ${query.limit} items)`;
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string): string {
+//     return `This action returns a #${id} cat`;
+//   }
+
+//   @Post()
+//   create(@Body() createCatDto: CreateCatDto): string {
+//     console.log(createCatDto);
+//     return `This action adds a new cat`;
+//   }
+
+//   @Put(':id')
+//   update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto): string {
+//     console.log(updateCatDto);
+//     return `This action updates a #${id} cat`;
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id') id: string): string {
+//     return `This action removes a #${id} cat`;
+//   }
+// }
