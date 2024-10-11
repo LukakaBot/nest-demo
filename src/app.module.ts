9eed99813f241d61d9b702f7b416758bfaba6fc4
Lukaka <monishine_code@163.com>
2024-10-11 15:45:43 +0800

    feat: 学习中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { DogsModule } from './dogs/dogs.module';
 import { ArticlesModule } from './articles/articles.module';
 import { ProductsModule } from './products/products.module';
+import {
+  LoggerMiddleware,
+  logger,
+} from './common/middleware/logger.middleware';
+import { CatsController } from './cats/cats.controller';
+import { DogsController } from './dogs/dogs.controller';
 
 @Module({
   imports: [CatsModule, DogsModule, ArticlesModule, ProductsModule],
@@ -13,6 +24,12 @@ import { ProductsModule } from './products/products.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply().forRoutes(AppController);
+    consumer
+      .apply(LoggerMiddleware, logger)
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST },
+      )
+      .forRoutes(CatsController, DogsController);
   }
 }
